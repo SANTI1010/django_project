b462c862bdfd72667a26f9eefd4fd8ba3ea0d3d2
scabrera <scabrera@siu.edu.ar>
2024-03-13 15:56:10 -0300

    Se agrega avance hasta el video 35

--- django_project_app/views.py
+++ django_project_app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, HttpResponse
+from servicios.models import Servicio
+
 
 # Create your views here.
 def home(request):
     return render(request,'django_project_app/home.html')
 
 def servicios(request):
-    return render(request,'django_project_app/servicios.html')
+    servicios = Servicio.objects.all()
+    return render(request,'django_project_app/servicios.html', {"servicios": servicios})
 
 def tienda(request):
     return render(request,'django_project_app/tienda.html')
