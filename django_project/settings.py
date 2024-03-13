11a0076bab324846d19a5b28d7c355978a7b3d1c
scabrera <scabrera@siu.edu.ar>
2024-03-13 15:14:41 -0300

    Se agrega avance hasta el video 34

--- django_project/settings.py
+++ django_project/settings.py
@@ -105,7 +105,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-eu'
 
 TIME_ZONE = 'UTC'
 
@@ -118,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- django_project_app/urls.py
+++ django_project_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
 from django_project_app import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.home, name='Home'),
@@ -8,4 +10,8 @@ urlpatterns = [
     path('tienda',views.tienda, name='Tienda'),
     path('blog',views.blog, name='Blog'),
     path('contacto',views.contacto, name='Contacto'),
+    
+
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
